15d1f0b80687308370596179951ee176e016b224
NartRamadan <kataz91@gmail.com>
2024-12-15 09:46:48 +0200

    שלד בסיסי

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const port = 3791;
+const app = express();
+
+// Middleware for handling JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
