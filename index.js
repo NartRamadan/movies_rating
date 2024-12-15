35c7a7bbc3d62af6d7514712130aa34d2afc14a0
NartRamadan <kataz91@gmail.com>
2024-12-15 09:53:42 +0200

    הוספת סרט הגדרת משתנים הגדרת מזהה מערך שיאחסן את הביקורות

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const port = 3791;
-const app = express();
+const express = require('express'); // ייבוא המודול express - משמש לבניית אפליקציות רשת ב-JS
+const port = 3791; // הגדרת מספר פורט שבו השרת יאזין לבקשות
+const app = express(); // יצירת מופע של אפליקציית Express
 
-// Middleware for handling JSON and URL-encoded data
+// Middleware עבור נתונים ב-JSON (כדי שהשרת יוכל להבין ולהגיב לבקשות JSON)
 app.use(express.json());
+
+// Middleware עבור נתונים URL-encoded (כמו נתונים שנשלחים ממסמכי HTML)
 app.use(express.urlencoded({ extended: false }));
 
+// הגדרת משתנים עבור מזהה כרגע והביקורות
+let curr_id = 1; // הגדרת מזהה חדש - יתחיל מ-1
+let reviews = []; // מערך שיאחסן את כל הביקורות
+let r1 = { id: 1, title: "LOTR", content: "one to rule them all" }; // יצירת ביקורת ראשונה עם מזהה, כותרת ותוכן
+curr_id++; // הגדלת מזהה הביקורת הבא
+reviews.push(r1); // הוספת הביקורת לרשימת הביקורות
 
-
-
-// Start the server
+// הפעלת השרת שיאזין לפניות על פורט 3791
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`); // הצגת הודעה שמציינת שהשרת מאזין על הפורט
 });
