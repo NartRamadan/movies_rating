8d603b90b2e658d42b67ed0a9cb20f6796b5f0d4
NartRamadan <kataz91@gmail.com>
2024-12-15 10:00:22 +0200

    שלד בסיסי עם הזנה ידנית של ביקורת ראשונה

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express'); // ייבוא המודול express - משמש לבניית אפליקציות רשת ב-JS
-const port = 3791; // הגדרת מספר פורט שבו השרת יאזין לבקשות
-const app = express(); // יצירת מופע של אפליקציית Express
-
-// Middleware עבור נתונים ב-JSON (כדי שהשרת יוכל להבין ולהגיב לבקשות JSON)
+const express = require('express');
+const port = 3791;
+const app = express();
 app.use(express.json());
 
-// Middleware עבור נתונים URL-encoded (כמו נתונים שנשלחים ממסמכי HTML)
-app.use(express.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
-// הגדרת משתנים עבור מזהה כרגע והביקורות
-let curr_id = 1; // הגדרת מזהה חדש - יתחיל מ-1
-let reviews = []; // מערך שיאחסן את כל הביקורות
-let r1 = { id: 1, title: "LOTR", content: "one to rule them all" }; // יצירת ביקורת ראשונה עם מזהה, כותרת ותוכן
-curr_id++; // הגדלת מזהה הביקורת הבא
-reviews.push(r1); // הוספת הביקורת לרשימת הביקורות
+let curr_id=1;
+let reviews=[];
+let r1={id:curr_id,title:"LOTR",content:"one to rule"};
+curr_id++;
+reviews.push(r1);
 
-// הפעלת השרת שיאזין לפניות על פורט 3791
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`); // הצגת הודעה שמציינת שהשרת מאזין על הפורט
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
 });
